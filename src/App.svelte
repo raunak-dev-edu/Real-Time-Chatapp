78ed8b289c0db154243ccf5eafe857edbbc6837e
raunak-dev-edu <raunak_2101cb47@iitp.ac.in>
2023-03-18 19:14:47 +0530

    modify style

--- src/App.svelte
+++ src/App.svelte
@@ -19,6 +19,20 @@
 </script>
 
 <style>
+	.title {
+		text-align: center;
+		font-size: 2rem;
+		font-weight: 600;
+		color: #8b0000;
+		margin-bottom: 30px;
+	}
+	.info{
+		text-align: center;
+		font-size: 1.2rem;
+		font-weight: 400;
+		color: rgb(0, 0, 99);
+		padding: 0px 30px;
+	}
 	main {
 		position: fixed;
 		top:50%;
@@ -27,7 +41,7 @@
 		width: 100%;
 		height: 100%;
 		max-width: 400px;
-		max-height: 400px;
+		max-height: 500px;
 		background: #fff;
 		border: 1px solid #eee;
 		border-radius: 5px;
@@ -56,9 +70,14 @@
 </style>
 
 <main>
+	<h1 class="title">Real-time Chat</h1>
+	{#if user}
+		<p>Welcome, {user.displayName}</p>
+	{/if}
 	{#if user}
 		<Chatroom user={user} logout ={logout}/>
 	{:else}
+	<p class="info">This is simple real-time-chat app. Please sign it to try.</p>
 		<div class="login-form">
 			<button on:click={login}>
 				<i class="fa-brands fa-google"></i>
